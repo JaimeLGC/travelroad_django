7b5308950b32aa22f7346c395a6002dce5b7bfd6
jaimelgc <jaimeleongarcia123@gmail.com>
2024-02-01 10:10:10 +0000

    redirecciones

--- places/views.py
+++ places/views.py
@@ -5,11 +5,19 @@ from .models import Place
 
 
 def index(request):
-    wished = Place.objects.filter(visited=False)
+    template = loader.get_template('places/index.html')
+    return HttpResponse(template.render(request))
+
+
+def visited(request):
     visited = Place.objects.filter(visited=True)
     template = loader.get_template('places/index.html')
-    context = {
-        'wished': wished,
-        'visited': visited,
-    }
+    context = {'visited': visited}
+    return HttpResponse(template.render(context, request))
+
+
+def wished(request):
+    wished = Place.objects.filter(visited=False)
+    template = loader.get_template('places/index.html')
+    context = {'wished': wished}
     return HttpResponse(template.render(context, request))
